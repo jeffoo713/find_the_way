1cc6a17d8899291bec1d2aa33c547801219d9210
Jeff Kim <jeff.kim713@gmail.com>
2023-12-26 00:31:35 -0800

    refactor LevelSelectItem

--- client/src/components/LevelSelectItem/LevelSelectItem.tsx
+++ client/src/components/LevelSelectItem/LevelSelectItem.tsx
@@ -4,54 +4,38 @@ import { GlobalContext } from '../../stateManagement/globalContext';
 import { GameConfigActionType, GameLevel } from '@/enums/gameConfig';
 
 type LevelSelectItemProps = {
-  gameLevel: GameLevel;
+  gameLevel: Exclude<GameLevel, GameLevel.NULL>;
+};
+
+const LEVEL_TITLES: Record<Exclude<GameLevel, GameLevel.NULL>, string> = {
+  [GameLevel.EASY]: 'EASY: 5X5',
+  [GameLevel.MODERATE]: 'MODERATE: 6X6',
+  [GameLevel.HARD]: 'HARD: 7X7',
+  [GameLevel.SUPER_HARD]: 'SUPER HARD: 8X8',
+};
+
+const LEVEL_STYLES: Record<Exclude<GameLevel, GameLevel.NULL>, string> = {
+  [GameLevel.EASY]: styles.level_easy,
+  [GameLevel.MODERATE]: styles.level_moderate,
+  [GameLevel.HARD]: styles.level_hard,
+  [GameLevel.SUPER_HARD]: styles.level_super_hard,
 };
 
 function LevelSelectItem(props: LevelSelectItemProps) {
   const { gameLevel } = props;
   const { dispatch } = useContext(GlobalContext);
 
-  const title = useMemo(() => {
-    switch (gameLevel) {
-      case GameLevel.EASY:
-        return 'EASY: 5X5';
-      case GameLevel.MODERATE:
-        return 'MODERATE: 6X6';
-      case GameLevel.HARD:
-        return 'HARD: 7X7';
-      case GameLevel.SUPER_HARD:
-        return 'SUPER HARD: 8X8';
-      default:
-        return 'EASY: 5X5';
-    }
-  }, [gameLevel]);
+  const title = useMemo(() => LEVEL_TITLES[gameLevel], [gameLevel]);
+  const style_class = useMemo(() => LEVEL_STYLES[gameLevel], [gameLevel]);
 
-  const style_class = useMemo(() => {
-    switch (gameLevel) {
-      case GameLevel.EASY:
-        return styles.level_easy;
-      case GameLevel.MODERATE:
-        return styles.level_moderate;
-      case GameLevel.HARD:
-        return styles.level_hard;
-      case GameLevel.SUPER_HARD:
-        return styles.level_super_hard;
-      default:
-        return styles.level_easy;
-    }
-  }, [gameLevel]);
+  const handleClick = () =>
+    dispatch({
+      type: GameConfigActionType.SET_GAME_LEVEL,
+      payload: { gameLevel },
+    });
 
   return (
-    <div
-      className={`${styles.level_select_item} ${style_class}`}
-      onClick={() => {
-        console.log('payload:', { gameLevel: gameLevel });
-        dispatch({
-          type: GameConfigActionType.SET_GAME_LEVEL,
-          payload: { gameLevel: gameLevel },
-        });
-      }}
-    >
+    <div className={`${styles.level_select_item} ${style_class}`} onClick={handleClick}>
       <h3>{title}</h3>
     </div>
   );
