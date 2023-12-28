b1e365978b3b0af05a8ce1256bfd0a018f664c39
Jeff Kim <jeff.kim713@gmail.com>
2023-12-28 00:38:50 -0800

    implement cell hover effect

--- client/src/components/Cell/Cell.tsx
+++ client/src/components/Cell/Cell.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import './cell.style.scss';
 import { BoardContext } from '../../context/BoardContext/BoadContextProvider';
 
@@ -9,12 +9,18 @@ type CellComp = {
 function Cell({ cellIndex }: CellComp) {
   const { shouldCellLightUp, handelClickCell, isWrongCell } = useContext(BoardContext);
 
+  const [isHovering, setIsHovering] = useState(false);
+
   return (
     <div
       className={`cell ${shouldCellLightUp(cellIndex) && 'correct_cell'} ${
         isWrongCell(cellIndex) && 'wrong_cell'
+      } ${
+        isHovering && !shouldCellLightUp(cellIndex) && !isWrongCell(cellIndex) && 'cell_hovered'
       }`}
       onClick={() => handelClickCell(cellIndex)}
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
     />
   );
 }
