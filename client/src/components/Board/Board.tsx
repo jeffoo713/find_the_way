72d1980b1a48ea4171d2dd40655ecdba4a37fa6d
Jeff Kim <jeff.kim713@gmail.com>
2024-01-15 23:45:34 -0800

    implement responsiveness in board

--- client/src/components/Board/Board.tsx
+++ client/src/components/Board/Board.tsx
@@ -1,10 +1,11 @@
 import { useContext } from 'react';
 import Cell from '../Cell/Cell';
-import './board.style.scss';
+import styles from './board.module.scss';
 import { useBoard } from '../../hooks/useBoard';
 import BoardTopBar from '../BoardTopBar/BoardTopBar';
 import { GlobalContext } from '../../context/GlobalContext/GlobalContext';
 import BoardContextProvider from '../../context/BoardContext/BoadContextProvider';
+import { useBoardResizeStyle } from '../../hooks/useBoardResizeStyle';
 
 function Board() {
   const {
@@ -16,13 +17,20 @@ function Board() {
   const boardState = useBoard(gameLevel);
   const { widthSize, board } = boardState;
 
+  const { boardResizeStyles } = useBoardResizeStyle();
+
   return (
     <BoardContextProvider {...boardState}>
-      <BoardTopBar />
-      <div className='board' style={{ gridTemplateColumns: `repeat(${widthSize}, 1fr)` }}>
-        {board.flat(1).map((_cell, cellIdx) => (
-          <Cell key={`cell-${cellIdx}`} cellIndex={cellIdx} />
-        ))}
+      <div className={styles.board_wrapper}>
+        <BoardTopBar />
+        <div
+          className={styles.board}
+          style={{ gridTemplateColumns: `repeat(${widthSize}, 1fr)`, ...boardResizeStyles }}
+        >
+          {board.flat(1).map((_cell, cellIdx) => (
+            <Cell key={`cell-${cellIdx}`} cellIndex={cellIdx} />
+          ))}
+        </div>
       </div>
     </BoardContextProvider>
   );
---
+++ client/src/hooks/useBoardResizeStyle.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import boardTopBarStyles from '../components/BoardTopBar/boardTopBar.module.scss';
+
+type BoardResizeStyle = {
+  width: `${number}px` | `${number}%`;
+};
+
+export const useBoardResizeStyle = () => {
+  const [boardResizeStyles, setBoardResizeStyles] = useState<BoardResizeStyle>({
+    width: '100%',
+  });
+
+  const resizeListener = () => {
+    const windowHeight = window.innerHeight;
+    const windowWidth = window.innerWidth;
+    const topBarHeight = document.querySelector(
+      `.${boardTopBarStyles.board_top_bar}`,
+    )!.clientHeight;
+
+    if (windowWidth >= windowHeight) {
+      setBoardResizeStyles({
+        width: `${windowHeight - topBarHeight - 16}px`,
+      });
+    } else {
+      setBoardResizeStyles({
+        width: '100%',
+      });
+    }
+  };
+
+  useEffect(() => {
+    addEventListener('resize', resizeListener);
+
+    return () => {
+      removeEventListener('resize', resizeListener);
+    };
+  }, []);
+
+  return { boardResizeStyles };
+};
